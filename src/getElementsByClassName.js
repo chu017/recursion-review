4986cb074c18da602f8902bc034985582612b2c9
calvin <calvinchenhupro@gmail.com>
2021-03-09 10:24:12 -0800

    Assignment complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,23 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // variable
+  var output = [];
+
+  // write an inner recursion
+  var recursionFunction = function (input) {
+    // check if input has a classlist and the className matches with the classList
+    if (input.classList && input.classList.contains(className)) {
+      output.push(input);
+    }
+    // iterate over the array to add nodes from childNodes if there is any
+    for (var i = 0; i < input.childNodes.length; i++) {
+      recursionFunction(input.childNodes[i]);
+    }
+  };
+  recursionFunction(document.body);
+
+  //return output
+  return output;
+
 };
